8ebedcde3abef8f2746b0cb3c633872b4298668a
supachanya6812032610347 <s6812032610347@email.kmutnb.ac.th>
2025-07-24 10:50:25 +0700

    Add files via upload

---
+++ coding_w04-01.c
@@ -0,0 +1,13 @@
+#include <stdio.h> //ใช้สำหรับเรียกฟังก์ชัน printf() จาก standard I/O library
+
+int main() {       // 'int' = Keyword, 'main' = Identifier, '()' = Operator
+    
+    printf("Hollo,\nC Tokens!\n");  // 'printf' = Identifier, ข้อความใน "" = Constant, '\n' =
+   
+    return 0;  // 'return' = Keyword, '0' = Constant, ';' = Operator
+}       
+/*Token Type   ใช้อยู่ในโค้ด
+Keyword int, return
+Identifier  main, printf
+Constant    "Hello,\n", "C tokens!\n", 0
+Operator     (), ;, =  */
